9c6814204807b19f948b1a47aa5cbff1fc2828b1
David Richard <davidrichard.ms@gmail.com>
2019-06-10 14:57:41 -0300

    Migrando aplicação para SPA

--- mae-david-store/SPA/config/app.php
+++ mae-david-store/SPA/config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+       // ' Tymon \ JWTAuth \ Providers \ JWTAuthServiceProvider ',
         /*
          * Application Service Providers...
          */
@@ -225,6 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        ' JWTAuth '  =>  ' Tymon \ JWTAuth \ Facades \ JWTAuth',
 
     ],
 
